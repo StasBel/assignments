535eaa214803bce8f21747c278c2fd5dd3b30987
StasBel <stasbelyaev96@gmail.com>
2015-11-24 23:56:50 +0300

    FirstCommit.

--- src/main/java/ru/spbau/mit/GameServerImpl.java
+++ src/main/java/ru/spbau/mit/GameServerImpl.java
@@ -1,25 +1,76 @@
 package ru.spbau.mit;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
 
 public class GameServerImpl implements GameServer {
-    public GameServerImpl(String gameClassName, Properties properties) {
-        throw new UnsupportedOperationException("TODO: implement");
+
+    private Game game;
+    private HashMap<String, Connection> connectionMap;
+    private int connectionsNumber;
+
+    public GameServerImpl(String gameClassName, Properties properties) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        connectionsNumber = 0;
+        connectionMap = new HashMap<>();
+        Class <?> gameClass = Class.forName(gameClassName);
+        game = (Game)gameClass.getConstructor(GameServer.class).newInstance(this);
+        for (String propertyName : properties.stringPropertyNames()) {
+            String methodName = "set" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1);
+            try {
+                Integer number = Integer.parseInt(properties.getProperty(propertyName));
+                gameClass.getMethod(methodName, Integer.class).invoke(game, number);
+            } catch (NumberFormatException e) {
+                gameClass.getMethod(methodName, String.class).invoke(game, properties.getProperty(propertyName));
+            }
+        }
     }
 
     @Override
     public void accept(final Connection connection) {
-        throw new UnsupportedOperationException("TODO: implement");
+        final String id = Integer.toString(connectionsNumber++);
+        synchronized (connectionMap) {
+            connectionMap.put(id, connection);
+        }
+        connection.send(id);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                game.onPlayerConnected(id);
+                while (!connection.isClosed()) {
+                    try {
+                        synchronized (connection) {
+                            if (!connection.isClosed()) {
+                                String message = connection.receive(100);
+                                game.onPlayerSentMsg(id, message);
+                            }
+                        }
+                    } catch (InterruptedException e) {
+                        //...
+                    }
+                }
+            }
+        }).start();
     }
 
     @Override
     public void broadcast(String message) {
-        throw new UnsupportedOperationException("TODO: implement");
+        synchronized (connectionMap) {
+            for (Connection connection : connectionMap.values()) {
+                if (!connection.isClosed()) {
+                    connection.send(message);
+                }
+            }
+        }
     }
 
     @Override
     public void sendTo(String id, String message) {
-        throw new UnsupportedOperationException("TODO: implement");
+        synchronized (connectionMap) {
+            Connection connection = connectionMap.get(id);
+            if (!connection.isClosed()) {
+                connection.send(message);
+            }
+        }
     }
 }
--- src/main/java/ru/spbau/mit/SumTwoNumbersGame.java
+++ src/main/java/ru/spbau/mit/SumTwoNumbersGame.java
@@ -4,17 +4,43 @@ import java.util.*;
 
 
 public class SumTwoNumbersGame implements Game {
+
+    private GameServer server;
+    private int number1, number2;
+    private Random random = new Random();
+
+    private void generateNumbers() {
+        number1 = Math.abs(random.nextInt());
+        number2 = Math.abs(random.nextInt());
+    }
+
+    private String numbersToString(){
+        return Integer.toString(number1) + " " + Integer.toString(number2);
+    }
+
     public SumTwoNumbersGame(GameServer server) {
-        throw new UnsupportedOperationException("TODO: implement");
+        this.server = server;
+        generateNumbers();
     }
 
     @Override
     public void onPlayerConnected(String id) {
-        throw new UnsupportedOperationException("TODO: implement");
+        server.sendTo(id, numbersToString());
     }
 
     @Override
     public void onPlayerSentMsg(String id, String msg) {
-        throw new UnsupportedOperationException("TODO: implement");
+        try {
+            if (Integer.parseInt(msg) == number1 + number2){
+                server.sendTo(id, "Right");
+                server.broadcast(id + " won");
+                generateNumbers();
+                server.broadcast(numbersToString());
+            } else {
+                server.sendTo(id, "Wrong");
+            }
+        } catch (NumberFormatException e) {
+            //...
+        }
     }
 }
